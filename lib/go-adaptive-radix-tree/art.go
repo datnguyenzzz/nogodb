b2b22e51fd3ad10829f30c5aae92d361f676035d
Nguyen Thanh Dat <datnguyen79198@gmail.com>
2025-05-06 23:59:43 +0700

    Add TODO
    Signed-off-by: Nguyen Thanh Dat <datnguyen79198@gmail.com>

--- lib/go-adaptive-radix-tree/art.go
+++ lib/go-adaptive-radix-tree/art.go
@@ -7,6 +7,33 @@ import (
 	gocontextawarelock "github.com/datnguyenzzz/nogodb/lib/go-context-aware-lock"
 )
 
+// TODO Implement Swizzlable Pointers onto Tree[V any]
+// Currently, the struct Tree[V Any] only supports in-memory storage because it relies on pointers to nodes.
+// Each node, in turn, contains multiple pointers to its children, forming the tree structure.
+// Due to this design, we cannot persist this structure to disk.
+// While we could add additional fields to the struct, e.g
+// type Tree[V any] struct {
+//	root internal.INode[V] // memory address - pointer to the root node
+//  block_id uint32 // for unswizzling
+//  offset uint32 // for unswizzling
+//	lock gocontextawarelock.ICtxLock
+//}
+// this approach might introduce unnecessary resource overhead (it requires extra 8 bytes per Node)
+// Therefore, we need to
+//    1. Enhance the current object Tree[V] to Swizzlable Pointers
+//    2. Implements:
+//      Serialization:
+//         When saving the data structure to disk, the pointers are "unswizzled," often by converting them into
+//         some form of identifier, such as an index or a unique ID, that is independent of memory addresses.
+//      Deserialization:
+//         When loading the data structure back into memory, the identifiers are used to locate the corresponding
+//         data objects, and the pointers are "swizzled" by replacing the identifiers with the actual memory
+//         addresses of the loaded objects.
+//
+// One major problem with implementing this (de)serialization process is that now we not only have to keep information
+// about the memory address of pointers but also if they are already in memory and if not,
+// what's the <Block,Offset> position they are stored.
+
 // Tree is an implementation of a radix tree with adaptive nodes.
 // It is also compatible with the interfaces of the popular radix tree library.
 // https://github.com/hashicorp/go-immutable-radix

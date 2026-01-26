0bbd55061eddc7fef35b9188201b8b636cd03c99
datnguyenzzz <datnguyen79198@gmail.com>
2026-01-26 15:01:55 +0100

    update concurrent benchmark
    Signed-off-by: datnguyenzzz <datnguyen79198@gmail.com>

--- lib/go-adaptive-radix-tree/art_benchmark_test.go
+++ lib/go-adaptive-radix-tree/art_benchmark_test.go
@@ -2,23 +2,41 @@ package go_adaptive_radix_tree
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/datnguyenzzz/nogodb/lib/go-adaptive-radix-tree/internal"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
 func BenchmarkInsert(b *testing.B) {
 	kvs := internal.SeedMapKVString(10_000_000)
 	ctx := context.Background()
 
-	for b.Loop() {
-		art := NewTree[string](ctx)
-		for _, kv := range kvs {
-			_, err := art.Insert(ctx, kv.Key, kv.Value)
-			require.NoError(b, err)
-		}
+	concurrencies := []int{1, 10, 20}
+
+	for _, concurrency := range concurrencies {
+		b.Run(fmt.Sprintf("BenchmarkInsert-%d", concurrency), func(b *testing.B) {
+			for b.Loop() {
+				eg, egCtx := errgroup.WithContext(ctx)
+				eg.SetLimit(concurrency)
+				art := NewTree[string](egCtx)
+				for _, kv := range kvs {
+					eg.Go(func() error {
+						_, err := art.Insert(egCtx, kv.Key, kv.Value)
+						require.NoError(b, err)
+						return err
+					})
+				}
+
+				err := eg.Wait()
+				require.NoError(b, err)
+			}
+		})
 	}
+
 }
 
 func BenchmarkGet(b *testing.B) {
@@ -29,11 +47,26 @@ func BenchmarkGet(b *testing.B) {
 		_, _ = art.Insert(ctx, kv.Key, kv.Value)
 	}
 
-	for b.Loop() {
-		for _, kv := range kvs {
-			_, err := art.Get(ctx, kv.Key)
-			require.NoError(b, err)
-		}
+	concurrencies := []int{1, 10, 20}
+
+	for _, concurrency := range concurrencies {
+		b.Run(fmt.Sprintf("BenchmarkGet-%d", concurrency), func(b *testing.B) {
+			for b.Loop() {
+				eg, egCtx := errgroup.WithContext(ctx)
+				eg.SetLimit(concurrency)
+				for _, kv := range kvs {
+					eg.Go(func() error {
+						v, err := art.Get(egCtx, kv.Key)
+						assert.Equal(b, kv.Value, v)
+						require.NoError(b, err)
+						return err
+					})
+				}
+
+				err := eg.Wait()
+				require.NoError(b, err)
+			}
+		})
 	}
 }
 
@@ -42,12 +75,28 @@ func BenchmarkInsertAndGet(b *testing.B) {
 	ctx := context.Background()
 	art := NewTree[string](ctx)
 
-	for b.Loop() {
-		for _, kv := range kvs {
-			_, err := art.Insert(ctx, kv.Key, kv.Value)
-			require.NoError(b, err)
-			_, err = art.Get(ctx, kv.Key)
-			require.NoError(b, err)
-		}
+	concurrencies := []int{1, 10, 20}
+
+	for _, concurrency := range concurrencies {
+		b.Run(fmt.Sprintf("BenchmarkInsertAndGet-%d", concurrency), func(b *testing.B) {
+			for b.Loop() {
+				eg, egCtx := errgroup.WithContext(ctx)
+				eg.SetLimit(concurrency)
+				for _, kv := range kvs {
+					eg.Go(func() error {
+						_, err := art.Insert(egCtx, kv.Key, kv.Value)
+						require.NoError(b, err)
+						v, err := art.Get(egCtx, kv.Key)
+						assert.Equal(b, kv.Value, v)
+						require.NoError(b, err)
+
+						return nil
+					})
+				}
+
+				err := eg.Wait()
+				require.NoError(b, err)
+			}
+		})
 	}
 }

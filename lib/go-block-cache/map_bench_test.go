0cf9ef6078b9d82b6563b63bfd3f8f8e0b4a0ce1
datnguyenzzz <datnguyen79198@gmail.com>
2026-02-11 16:32:40 +0100

    update go-block-cache benchmark
    Signed-off-by: datnguyenzzz <datnguyen79198@gmail.com>

--- lib/go-block-cache/map_bench_test.go
+++ lib/go-block-cache/map_bench_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/dgraph-io/ristretto/v2"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -17,277 +18,89 @@ const (
 // Sync
 
 func Benchmark_Ristretto_Cache_Add_Read(b *testing.B) {
-	b.StopTimer()
 	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
 		NumCounters: 40_000, // 5x estimated nodes
 		MaxCost:     2 * MiB,
 		BufferItems: 64,
 	})
+	require.NoError(b, err)
 	defer cache.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_ = cache.Set(uint64(i), randomBytes(valueSize), valueSize)
-		_, _ = cache.Get(uint64(i))
+	for b.Loop() {
+		k, v := uint64(rand.Intn(100_000)), randomBytes(valueSize)
+		_ = cache.Set(k, v, valueSize)
+		_, _ = cache.Get(k)
 	}
-	b.ReportAllocs()
 	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_Ristretto_Cache_Add(b *testing.B) {
-	b.StopTimer()
-	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
-		NumCounters: 40_000, // 5x estimated nodes
-		MaxCost:     2 * MiB,
-		BufferItems: 64,
-	})
-	defer cache.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_ = cache.Set(uint64(i), randomBytes(valueSize), valueSize)
-	}
-	b.ReportAllocs()
-	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_Ristretto_Cache_Read(b *testing.B) {
-	b.StopTimer()
-	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
-		NumCounters: 40_000, // 5x estimated nodes
-		MaxCost:     2 * MiB,
-		BufferItems: 64,
-	})
-	defer cache.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		_ = cache.Set(uint64(i), randomBytes(valueSize), valueSize)
-	}
-	cache.Wait()
-
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_, _ = cache.Get(uint64(i))
-	}
-
-	b.ReportAllocs()
-	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
+	b.ReportMetric(float64(cache.Metrics.Ratio()), "hit_ratio")
 }
 
 // ASync
 
 func Benchmark_Ristretto_Cache_Add_Read_Async(b *testing.B) {
-	b.StopTimer()
 	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
 		NumCounters: 40_000, // 5x estimated nodes
 		MaxCost:     2 * MiB,
 		BufferItems: 64,
 	})
+	require.NoError(b, err)
 	defer cache.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	b.StartTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		var i uint64
-		for pb.Next() {
-			_ = cache.Set(i, randomBytes(valueSize), valueSize)
-			_, _ = cache.Get(i)
-
-			i += 1
-		}
-	})
-	b.ReportAllocs()
-	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_Ristretto_Cache_Add_Async(b *testing.B) {
-	b.StopTimer()
-	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
-		NumCounters: 40_000, // 5x estimated nodes
-		MaxCost:     2 * MiB,
-		BufferItems: 64,
-	})
-	defer cache.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	var fileNum int64
-	b.StartTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		atomic.AddInt64(&fileNum, 1)
-		var i uint64
-		for pb.Next() {
-			_ = cache.Set(i, randomBytes(valueSize), valueSize)
-
-			i += 1
-		}
-	})
-	b.ReportAllocs()
-	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_Ristretto_Cache_Read_Async(b *testing.B) {
-	b.StopTimer()
-	cache, err := ristretto.NewCache(&ristretto.Config[uint64, []byte]{
-		NumCounters: 40_000, // 5x estimated nodes
-		MaxCost:     2 * MiB,
-		BufferItems: 64,
-	})
-	defer cache.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		_ = cache.Set(uint64(i), randomBytes(valueSize), valueSize)
-	}
-
-	cache.Wait()
-
-	var counter uint64
-	b.StartTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			i := atomic.AddUint64(&counter, 1) - 1
-			_, _ = cache.Get(i)
+			k, v := uint64(rand.Intn(100_000)), randomBytes(valueSize)
+			_ = cache.Set(k, v, valueSize)
+			_, _ = cache.Get(k)
 		}
 	})
-	b.ReportAllocs()
 	b.ReportMetric(float64(2*MiB-cache.RemainingCost()), "mem_footprint_in_bytes")
+	b.ReportMetric(float64(cache.Metrics.Ratio()), "hit_ratio")
 }
 
 // NogoDB - block-cache
 
 // Sync
 
-func Benchmark_NogoDB_Cache_Add_Read(b *testing.B) {
-	b.StopTimer()
+func Benchmark_NogoDB_LRU_Cache_Add_Read(b *testing.B) {
 	c := NewMap(
 		WithCacheType(LRU),
 		WithMaxSize(2*MiB),
 	)
 
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_ = c.Set(0, uint64(i), randomBytes(valueSize))
-		_, _ = c.Get(0, uint64(i))
+	for b.Loop() {
+		k, v := uint64(rand.Intn(100_000)), randomBytes(valueSize)
+		_ = c.Set(0, k, v)
+		_, _ = c.Get(0, k)
 	}
-	b.ReportAllocs()
 	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_NogoDB_Cache_Add(b *testing.B) {
-	b.StopTimer()
-	c := NewMap(
-		WithCacheType(LRU),
-		WithMaxSize(2*MiB),
-	)
-
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_ = c.Set(0, uint64(i), randomBytes(valueSize))
-	}
-	b.ReportAllocs()
-	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_NogoDB_Cache_Read(b *testing.B) {
-	b.StopTimer()
-	c := NewMap(
-		WithCacheType(LRU),
-		WithMaxSize(2*MiB),
-	)
-
-	for i := 0; i < b.N; i++ {
-		_ = c.Set(0, uint64(i), randomBytes(valueSize))
-	}
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		_, _ = c.Get(0, uint64(i))
-	}
-
-	b.ReportAllocs()
-	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
+	b.ReportMetric(float64(c.GetStats().statHit)/float64(c.GetStats().statHit+c.GetStats().statMiss), "hit_ratio")
 }
 
 // ASync
 
-func Benchmark_NogoDB_Cache_Add_Read_Async(b *testing.B) {
-	b.StopTimer()
+func Benchmark_NogoDB_LRU_Cache_Add_Read_Async(b *testing.B) {
 	c := NewMap(
 		WithCacheType(LRU),
 		WithMaxSize(2*MiB),
 	)
 
 	var fileNum int64
-	b.StartTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		atomic.AddInt64(&fileNum, 1)
-		var i uint64
 		for pb.Next() {
-			_ = c.Set(0, i, randomBytes(valueSize))
-			_, _ = c.Get(0, i)
-
-			i += 1
+			k, v := uint64(rand.Intn(100_000)), randomBytes(valueSize)
+			_ = c.Set(0, k, v)
+			_, _ = c.Get(0, k)
 		}
 	})
-	b.ReportAllocs()
 	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_NogoDB_Cache_Add_Async(b *testing.B) {
-	b.StopTimer()
-	c := NewMap(
-		WithCacheType(LRU),
-		WithMaxSize(2*MiB),
-	)
-
-	var fileNum int64
-	b.StartTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		atomic.AddInt64(&fileNum, 1)
-		var i uint64
-		for pb.Next() {
-			_ = c.Set(0, i, randomBytes(valueSize))
-			i += 1
-		}
-	})
-	b.ReportAllocs()
-	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
-}
-
-func Benchmark_NogoDB_Cache_Read_Async(b *testing.B) {
-	b.StopTimer()
-	c := NewMap(
-		WithCacheType(LRU),
-		WithMaxSize(2*MiB),
-	)
-
-	for i := 0; i < b.N; i++ {
-		_ = c.Set(0, uint64(i), randomBytes(valueSize))
-	}
-	var counter uint64
-	b.StartTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			i := atomic.AddUint64(&counter, 1) - 1
-			_, _ = c.Get(0, i)
-		}
-	})
-	b.ReportAllocs()
-	b.ReportMetric(float64(c.GetInUsed()), "mem_footprint_in_bytes")
+	b.ReportMetric(float64(c.GetStats().statHit)/float64(c.GetStats().statHit+c.GetStats().statMiss), "hit_ratio")
 }
 
 func randomBytes(sz int) []byte {

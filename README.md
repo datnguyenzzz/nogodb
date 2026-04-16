e6ffe321c4b7d7b6f1f3eede653c1adc94338590
datnguyenzzz <datnguyen79198@gmail.com>
2026-04-16 22:49:53 +0200

    implement KVBlock iterator
    Signed-off-by: datnguyenzzz <datnguyen79198@gmail.com>

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ### NogoDB
 
->_**Disclaimer**: This project is under active development and deliberately crafted by human ingenuity rather than AI. I have no desire to let AI steal the enjoyment of building things by hand. Although AI can produce code faster and boost "__productivity__", that is beside the point. This project is meant to entertain me and deepen my understanding of the database world, not to be rushed into a commercial product :)_
+>_**Disclaimer**: This project is under active development and deliberately crafted by human rather than AI. I have no desire to let AI steal the enjoyment of building things by hand. Although AI can produce code faster and boost "__productivity__", that is beside the point. This project is meant to entertain me and deepen my understanding of the database world, not to be rushed into a commercial product :)_
 
 <p align="center">
     <img width="300" height="200" alt="dg1m4qm1ksrmr0cxh1e9d67htr_result_0" src="https://github.com/user-attachments/assets/1815d820-f8c1-45db-8504-601033e6caaf" />
 </p>
 
 
-NogoDB is an analytics database management system.
+NogoDB is an OLAP database management system.
 
+I’m not sure what else to add to the description at the moment, as it’s still a work in progress. However, I believe the a brief quote above already gives you all a clear idea of what I’m building :) 
 
 
 
---
+++ lib/go-sstable/block/col_block/kv_block_iter.go
@@ -0,0 +1,126 @@
+package colblock
+
+import (
+	"github.com/datnguyenzzz/nogodb/lib/go-bytesbufferpool/predictable_size"
+	layoutcodex "github.com/datnguyenzzz/nogodb/lib/go-sstable/block/col_block/codex/layout_codex"
+	rawbytescodex "github.com/datnguyenzzz/nogodb/lib/go-sstable/block/col_block/codex/raw_bytes_codex"
+	"github.com/datnguyenzzz/nogodb/lib/go-sstable/common"
+	"go.uber.org/zap"
+)
+
+type KVBlockIter struct {
+	bp   *predictable_size.PredictablePool
+	keys *rawbytescodex.RawBytesDecoder
+	// values store the encoded blochHandle
+	values  *rawbytescodex.RawBytesDecoder
+	currRow uint32 // used for iterating over the block
+	closed  bool
+}
+
+func (i *KVBlockIter) SeekGTE(key []byte) *common.InternalKV {
+	panic("KVBlockIter can't do this")
+}
+
+func (i *KVBlockIter) SeekPrefixGTE(prefix, key []byte) *common.InternalKV {
+	panic("KVBlockIter can't do this")
+}
+
+func (i *KVBlockIter) SeekLTE(key []byte) *common.InternalKV {
+	panic("KVBlockIter can't do this")
+}
+
+// First moves the iterator the first key/value pair.
+func (i *KVBlockIter) First() *common.InternalKV {
+	i.currRow = 0
+	return i.toKv()
+}
+
+// Last moves the iterator the last key/value pair.
+func (i *KVBlockIter) Last() *common.InternalKV {
+	i.currRow = i.keys.Rows() - 1
+	return i.toKv()
+}
+
+// Next moves the iterator to the next key/value pair
+func (i *KVBlockIter) Next() *common.InternalKV {
+	if i.currRow == i.keys.Rows()-1 {
+		return nil
+	}
+
+	i.currRow = i.currRow + 1
+	return i.toKv()
+}
+
+// Prev moves the iterator to the previous key/value pair.
+func (i *KVBlockIter) Prev() *common.InternalKV {
+	if i.currRow == 0 {
+		return nil
+	}
+
+	i.currRow = i.currRow - 1
+	return i.toKv()
+}
+
+func (i *KVBlockIter) Close() error {
+	i.closed = true
+	i.currRow = 0
+	i.keys = nil
+	i.values = nil
+	return nil
+}
+
+func (i *KVBlockIter) IsClosed() bool {
+	return i.closed
+}
+
+// toKv converts the current row to the InternalKV
+func (i *KVBlockIter) toKv() *common.InternalKV {
+	iKv := &common.InternalKV{
+		K: common.InternalKey{},
+	}
+	iKv.K.UserKey = i.keys.Get(i.currRow)
+	buf := i.values.Get(i.currRow)
+	v := common.NewBlankInternalLazyValue(common.ValueFromBuffer)
+	v.ReserveBuffer(i.bp, len(buf))
+	if err := v.SetBufferValue(buf[:]); err != nil {
+		zap.L().Error("failed to set value", zap.Error(err))
+	}
+	iKv.V = v
+	return iKv
+}
+
+func NewKVBlockIter(
+	bp *predictable_size.PredictablePool,
+	cp common.IComparer,
+	data *common.InternalLazyValue,
+) *KVBlockIter {
+	kv := &KVBlockIter{
+		bp: bp,
+	}
+	decoder := layoutcodex.NewLayoutDecoder(data.Value())
+
+	var ok bool
+	kv.keys, ok = layoutcodex.Decode(
+		cp,
+		decoder,
+		0,
+		rawbytescodex.NewRawBytesDecoder,
+	).(*rawbytescodex.RawBytesDecoder)
+	if !ok {
+		panic("NewKVBlockIter, failed to assert to RawBytesDecoder")
+	}
+
+	kv.keys, ok = layoutcodex.Decode(
+		cp,
+		decoder,
+		1,
+		rawbytescodex.NewRawBytesDecoder,
+	).(*rawbytescodex.RawBytesDecoder)
+	if !ok {
+		panic("NewKVBlockIter, failed to assert to RawBytesDecoder")
+	}
+
+	return kv
+}
+
+var _ common.InternalIterator = (*KVBlockIter)(nil)
